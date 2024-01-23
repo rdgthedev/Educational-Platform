3ca5a688987b1a2e957d9d80a308cdad8f12d8c1
rdgthedev <rodrigo_ferreira996@hotmail.com>
2024-01-23 20:11:18 -0300

    Adicionando Projeto

---
+++ EducationalPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EducationalPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
